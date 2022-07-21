2b798781bd695a50463dadcab9e87350f3b3beab
ZYTRUST SA <ztmobile@zytrust.com>
2022-07-21 10:53:37 -0500

    0.1.1

--- zy_encrypt_util/Classes/ZyEncrypt.swift
+++ zy_encrypt_util/Classes/ZyEncrypt.swift
@@ -106,6 +106,36 @@ public class ZyEncrypt{
         
         return Array(arr[from..<to]) + [T](repeating: 0, count: padding)
     }
+    
+    
 
 }
 
+extension String {
+    func index(from: Int) -> Index {
+        return self.index(startIndex, offsetBy: from)
+    }
+    
+    func substring(from: Int) -> String {
+        let fromIndex = index(from: from)
+        return String(self[fromIndex...])
+    }
+    
+    func substring(to: Int) -> String {
+        let toIndex = index(from: to)
+        return String(self[..<toIndex])
+    }
+    
+    func substring(with r: Range<Int>) -> String {
+        let startIndex = index(from: r.lowerBound)
+        let endIndex = index(from: r.upperBound)
+        return String(self[startIndex..<endIndex])
+    }
+    
+    
+    
+    func toBase64() -> String {
+        return Data(self.utf8).base64EncodedString()
+    }
+    
+}
